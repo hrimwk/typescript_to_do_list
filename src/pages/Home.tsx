a69fb6d02d94b2e39e587e9bb366c261103dea91
hrim <hrim.wk@gmail.com>
2022-11-07 12:41:11 +0900

    [Add] 기본 ui내용, common scss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
 import '../assets/styles/home.scss';
 
 function Home() {
-  return <>home</>;
+  return (
+    <div className="container">
+      <h1>TO DO LIST</h1>
+      <section className="list-container">
+        <div className="list d-flex">
+          <input type="checkbox" />
+          <p className="mr50">내용이 들어갑니다</p>
+          <span>삭제</span>
+        </div>
+      </section>
+      <section className="add-container">
+        <input className="add-content" />
+        <span className="add">+</span>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
