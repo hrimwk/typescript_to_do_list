6177194429ab6218f250535ef5d683a76b4124d8
hrim <hrim.wk@gmail.com>
2022-11-07 18:17:43 +0900

    [Add] 내용 추가 삭제 기능

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,53 @@
+import React, { useState } from 'react';
+import { Value } from 'sass';
 import '../assets/styles/home.scss';
 
 function Home() {
+  interface List {
+    id: number;
+    value: string;
+  }
+  const [lists, setList] = useState<List[] | null>([]);
+  const [todoInput, setInput] = useState<string>('');
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  const deleteList = (e: React.MouseEvent<HTMLSpanElement>) => {
+    console.log('?', e.currentTarget.id);
+    setList(lists.filter((todo) => todo.id.toString() !== e.currentTarget.id));
+  };
+  const addList = () => {
+    let newList = lists.map((data, idx) => {
+      return { id: idx, value: data.value };
+    });
+    setList([...newList, { id: newList.length, value: todoInput }]);
+    setInput('');
+  };
   return (
     <div className="container">
-      <h1>TO DO LIST</h1>
+      <h1 className="title">TO DO LIST</h1>
       <section className="list-container">
-        <div className="list d-flex">
-          <input type="checkbox" />
-          <p className="mr50">내용이 들어갑니다</p>
-          <span>삭제</span>
+        <div className="list">
+          <ul>
+            {lists.map((data) => {
+              return (
+                <li className="d-flex" key={data.id}>
+                  <input type="checkbox" className="check-box" />
+                  <label className="mr50">{data.value}</label>
+                  <span className="delete" onClick={deleteList} id={`${data.id}`}>
+                    삭제
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </section>
-      <section className="add-container">
-        <input className="add-content" />
-        <span className="add">+</span>
+      <section className="add-container d-flex">
+        <input className="add-content" onChange={inputChange} value={todoInput} />
+        <span className="add" onClick={addList}>
+          +
+        </span>
       </section>
     </div>
   );
